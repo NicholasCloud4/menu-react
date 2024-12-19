2bda569fd8866ef87621fdfce82394df47351973
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-19 13:25:35 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import ReactDOM from "react-dom/client";
+import Menu from "./components/Menu/Menu";
+import MenuButton from "./components/Menu/MenuButton";
+import MenuDropdown from "./components/Menu/MenuDropdown";
+
+function App() {
+  /**
+   * Challenge:
+   * 1. Convert the Menu component to use props.children
+   *    instead of taking an `items` prop. (We'll update
+   *    the MenuButton and MenuDropdown components later.)
+   *    See note inside the Menu.js file for more info
+   *
+   * 2. import MenuButton and MenuDropdown into THIS file
+   *    and render them as children of the Menu component.
+   *    Remember to pass the buttonText and items array to
+   *    the components that need those props to function.
+   *    (We'll also be updating that soon!)
+   *
+   * NOTE: The functionality of the menu will be broken after
+   * these changes, but that's okay! As such, don't worry
+   * about moving the state or toggle function from the Menu;
+   * there's more we need to learn before we can do that.
+   */
+
+  return (
+    <>
+      <Menu>
+        <MenuButton>Sports</MenuButton>
+        <MenuDropdown items={["Tennis", "Pickleball", "Racquetball", "Squash"]} />
+      </Menu>
+    </>
+  );
+}
+
+export default App;
