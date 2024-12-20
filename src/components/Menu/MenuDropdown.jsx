7f63f5c1801e2e059ae6cecc7abd34e7dccc682f
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-20 10:41:22 -0500

    Diving a bit deeper into React.Children.map

--- src/components/Menu/MenuDropdown.jsx
+++ src/components/Menu/MenuDropdown.jsx
@@ -1,9 +1,17 @@
 import React from "react"
 
-export default function MenuDropdown({ children, open }) {
+export default function MenuDropdown({ children, open, toggle }) {
+    console.log("MENU DROPDOWN", open, toggle)
+    /** Discovery Challenge:
+     * Make it so the children of MenuDropdown also have
+     * access to toggle and open.
+     */
     return open === true ? (
         <div className="menu-dropdown">
-            {children}
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, { toggle, open })
+            })}
+            {/* {children} */}
         </div>
     ) : null
 }
