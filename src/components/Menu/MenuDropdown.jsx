6c8000ed0b8eb3a37273f6f7a1e3a2171ccfe00b
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-19 20:07:19 -0500

    React.Children

--- src/components/Menu/MenuDropdown.jsx
+++ src/components/Menu/MenuDropdown.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 
-export default function MenuDropdown({ children }) {
-    return (
+export default function MenuDropdown({ children, open }) {
+    return open === true ? (
         <div className="menu-dropdown">
             {children}
         </div>
-    )
+    ) : null
 }
